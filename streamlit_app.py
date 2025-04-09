ea5f7b8dece2d08a23cdefb897cc76f5354f2a25
alistair-snap <alistair.snap@gmail.com>
2025-04-09 09:19:12 +0100

    Update streamlit_app.py
    Updated to current version

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,9 @@ helpful_links = [
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be: ', name_on_order)
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -20,7 +23,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients',
-    my_dataframe
+    my_dataframe,
+    max_selections=5
 )
 
 if ingredients_list:
@@ -30,8 +34,8 @@ if ingredients_list:
 
     st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
 
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
